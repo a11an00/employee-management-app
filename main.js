d699bfcce85aeeeb09f4ccc791cb1f18355d6371
Allan Kiptanui <akiptanui17@gmail.com>
2026-03-29 22:51:33 +0300

    view contact addition

---
+++ main.js
@@ -0,0 +1,14 @@
+function edit() {
+    alert("EDITING CONTACT!!!");
+}
+function deleteContact() {
+    confirm("Are you sure you want to delete this contact?");
+}
+function viewContact() {
+    alert("VIEWING CONTACT!!!");
+}
+
+function addContact() {
+    alert("ADDING CONTACT!!!");
+    
+}
